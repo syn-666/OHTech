0b8b28092cf04bbc8e100c0d63495f40d29a571a
syn <1599386309@qq.com>
2021-07-04 18:26:15 +0800

    init

---
+++ App.vue
@@ -0,0 +1,26 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			if(!wx.cloud){
+				console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+			}else{
+				wx.cloud.init({
+					traceUser: true,
+					env: 'onehands-8gt9feadad3b9139'
+				})
+			}
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style lang="scss">
+	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+	@import "uview-ui/index.scss";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="content">
+		<u-button >默认按钮</u-button>
+		<u-button type="primary">主要按钮</u-button>
+		<u-button type="success">成功按钮</u-button>
+		<u-button type="info">信息按钮</u-button>
+		<u-button type="warning">警告按钮</u-button>
+		<u-button type="error">危险按钮</u-button>
+	</view>
+</template>
+
+<script>
+	const db = wx.cloud.database()
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			db.collection('test').doc('28ee4e3e60e1806f279de3f82108dc79').get()
+			.then(res=>{
+				console.log(res)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
